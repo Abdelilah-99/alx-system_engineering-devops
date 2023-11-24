be1dcf591ecb0a81213baa59ece0eb0a77316081
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-11-24 22:36:34 +0100

    task01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# Installs Flask from pip3
-# esuring that we had 2.1.0 -v
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
